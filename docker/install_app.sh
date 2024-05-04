f653521f6944bbe621e1943fa4bdbc07974d553e
Daulet Lepessov <dauletlepessov@Daulets-Laptop.local>
2024-05-04 12:54:18 +0500

    Added docker and made migrations

--- docker/install_app.sh
+++ docker/install_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose exec -T php-fpm chmod -R 777 storage/ bootstrap/
-docker-compose exec -T php-fpm cp .env.deveopment .env
+docker-compose exec -T php-fpm cp .env.example .env
 docker-compose exec -T php-fpm composer install
 docker-compose exec -T php-fpm composer update --lock
 docker-compose exec -T php-fpm php artisan optimize:clear
